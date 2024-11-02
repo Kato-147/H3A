d44dbee22c405522fa4aa71bda1e0f5ede6450f3
Kato-147 <oroki147@gmail.com>
2024-11-02 19:58:38 +0700

    fix bug import toast

--- components/layout/course_sections/info.tsx
+++ components/layout/course_sections/info.tsx
@@ -7,7 +7,7 @@ import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
 import { Separator } from "@/components/ui/separator"
-import { toast } from "@/hooks/use-toast"
+import { toast } from "../../../hooks/use-toast"
 
 export default function Info() {
 
